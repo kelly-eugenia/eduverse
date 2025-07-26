3de703fd2760072b6a99494c8d392316a254936e
Kelly S. <kelly.eugenia35@gmail.com>
2025-07-27 03:52:18 +1000

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/cos30043/s104475686/Project/",
-  plugins: [vue()],
-});
