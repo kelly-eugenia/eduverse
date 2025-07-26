8fc1a8863543104b93aea283fe1a4bb6c062c822
Kelly S. <kelly.eugenia35@gmail.com>
2025-07-27 03:51:28 +1000

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.vue";
-import { store } from "./store";
-import router from "./router";
-
-createApp(App).use(store).use(router).mount("#app");
